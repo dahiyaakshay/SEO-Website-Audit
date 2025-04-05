f1fde934fd68e9a1b18e06e175d08e87983241ad
Akshay Dahiya <akddme@gmail.com>
2025-04-06 02:48:01 +0530

    __init__.py

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -1,1 +1,21 @@
-# Package initialization
+# Package initialization for analyzers module
+
+# Import analyzers for easier access
+from .seo_analyzer import SEOAnalyzer
+from .performance_analyzer import PerformanceAnalyzer
+from .content_analyzer import ContentAnalyzer
+from .accessibility_analyzer import AccessibilityAnalyzer
+from .security_analyzer import SecurityAnalyzer
+from .design_analyzer import DesignAnalyzer
+
+# Package metadata
+__all__ = [
+    'SEOAnalyzer',
+    'PerformanceAnalyzer',
+    'ContentAnalyzer',
+    'AccessibilityAnalyzer',
+    'SecurityAnalyzer',
+    'DesignAnalyzer'
+]
+
+__version__ = '0.1.0'
