1927c7fb11a92fabfed643655581f727e3071fe6
Akshay Dahiya <akddme@gmail.com>
2025-04-06 18:53:17 +0530

    init.py

--- utils/init.py
+++ utils/init.py
@@ -1,1 +1,15 @@
+# Package initialization for utils module
 
+# Import utility classes for easier access
+from .scraper import WebScraper
+from .report_generator import ReportGenerator
+from .ai_integration import TogetherAIClient
+
+# Package metadata
+__all__ = [
+    'WebScraper',
+    'ReportGenerator',
+    'TogetherAIClient'
+]
+
+__version__ = '0.1.0'
